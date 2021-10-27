3ac90abf353b8a304d8479931166af5c7dc7273f
Rodrigo <rodrigo.kartcheski@gmail.com>
2021-10-27 14:46:48 -0300

    atualização do repositorio

---
+++ jenkins_tst/exec_job.bat
@@ -0,0 +1,3 @@
+C:
+"C:\pentaho\pdi-ce-8.3.0.0-371\data-integration\Kitchen.bat" /file:"C:\pentaho\repositorio\cases_pdi\jenkins_tst\primeiro_job_pdi.kjb" /level:Basic
+pause
---
+++ jenkins_tst/exec_ktr.bat
@@ -0,0 +1,3 @@
+C:
+"C:\pentaho\pdi-ce-8.3.0.0-371\data-integration\Pan.bat" /file:"C:\pentaho\repositorio\cases_pdi\jenkins_tst\primeiro_ktr_pdi.ktr" /level:Basic
+pause
