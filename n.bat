7277530be9fed48f934b7ad7585b6225d320bf3d
Rodrigo <rodrigo.kartcheski@gmail.com>
2021-10-27 21:14:19 -0300

    autoCommit 20211027.211419

--- n.bat
+++ n.bat
@@ -1,5 +1,5 @@
 cd C:\pentaho\repositorio\cases_pdi
 git add --all
 git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
-git push
+git push -u origin master
 exit
