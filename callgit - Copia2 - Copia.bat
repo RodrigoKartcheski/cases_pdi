0a468acd954bf41306be76ff01ee362bec3afd63
Rodrigo <rodrigo.kartcheski@gmail.com>
2021-10-29 08:44:41 -0300

    autoCommit 20211029. 84441

---
+++ callgit - Copia2 - Copia.bat
@@ -0,0 +1,6 @@
+echo
+cd C:\repositorio
+git add -A
+git diff-index --quiet HEAD || git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+git push -u origin master
+exit
